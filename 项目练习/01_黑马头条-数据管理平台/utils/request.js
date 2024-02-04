1586ca379f3f99a6fd91f9574f9cb977e647c781
PYY <1832478242@qq.com>
2024-02-04 16:52:39 +0800

    项目学习-响应拦截器 	new file:   "3_\346\241\206\346\236\266\345\211\215\347\275\256(AJAX-Node.js-Webpack-Git)/1_AJAX/2_images/axios\345\223\215\345\272\224\346\213\246\346\210\252\345\231\250.png" 	new file:   "3_\346\241\206\346\236\266\345\211\215\347\275\256(AJAX-Node.js-Webpack-Git)/1_AJAX/2_images/axios\350\257\267\346\261\202\346\213\246\346\210\252\345\231\250.png" 	new file:   "3_\346\241\206\346\236\266\345\211\215\347\275\256(AJAX-Node.js-Webpack-Git)/1_AJAX/2_images/\351\252\214\350\257\201\347\240\201\347\231\273\345\275\225\346\265\201\347\250\213.png" 	modified:   5_Note/Ajax.md 	modified:   "\351\241\271\347\233\256\347\273\203\344\271\240/01_\351\273\221\351\251\254\345\244\264\346\235\241-\346\225\260\346\215\256\347\256\241\347\220\206\345\271\263\345\217\260/page/login/index.js" 	modified:   "\351\241\271\347\233\256\347\273\203\344\271\240/01_\351\273\221\351\251\254\345\244\264\346\235\241-\346\225\260\346\215\256\347\256\241\347\220\206\345\271\263\345\217\260/utils/auth.js" 	modified:   "\351\241\271\347\233\256\347\273\203\344\271\240/01_\351\273\221\351\251\254\345\244\264\346\235\241-\346\225\260\346\215\256\347\256\241\347\220\206\345\271\263\345\217\260/utils/request.js"

--- 项目练习/01_黑马头条-数据管理平台/utils/request.js
+++ 项目练习/01_黑马头条-数据管理平台/utils/request.js
@@ -1,2 +1,15 @@
 // axios 公共配置
 // 基地址
+axios.defaults.baseURL = 'http://geek.itheima.net'; // 设置默认的API接口基地址
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  // 统一携带 token 令牌字符串在请求头
+  const token  = localStorage.getItem('token')
+  token && (config.headers.Authorization = `Bearer ${token}`)
+  return config
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error)
+})
